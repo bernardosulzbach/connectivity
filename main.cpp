709adcdc918cf963faaf032f8dee71ba1374be30
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2020-05-20 07:36:37 -0300

    Updates some of the C++ and the usage interface

--- main.cpp
+++ main.cpp
@@ -10,20 +10,23 @@
 #include <string>
 #include <thread>
 #include <type_traits>
+#include <utility>
 
 using U16 = std::uint16_t;
 using U64 = std::uint64_t;
 
-static const char *filename = "records.bin";
-static const char *url = "http://example.com";
-static const U64 samplingInterval = 30;
+using F32 = float;
+
+constexpr static std::chrono::duration<F32> RequestInterval{30.0f};
+
+constexpr static auto DefaultCommand = "./connectivity-monitor";
 
 enum class Result : U16 { Undefined = 0, Success = 1, Failure = 2 };
 
 struct Period {
   std::string name;
   U64 duration;
-  Period(std::string name, U64 duration) : name(name), duration(duration) {}
+  Period(std::string name, U64 duration) : name(std::move(name)), duration(duration) {}
 };
 
 struct Record {
@@ -58,7 +61,7 @@ std::string toString(double value, int digits) {
   return ss.str();
 }
 
-void run(const std::string &filename) {
+void run(const std::string &filename, const std::string &url) {
   Record record;
   record.timestamp = std::time(nullptr);
   try {
@@ -80,17 +83,27 @@ void run(const std::string &filename) {
   file.flush();
 }
 
-int main(int argc, char **argv) {
+void printUsage() {
+  std::cout << "Use: " << DefaultCommand << " <FILENAME> <ACTION> [URL]" << '\n';
+  std::cout << "Actions are --dump, --stats, --monitor <URL>." << '\n';
+}
+
+void actionDispatcher(const std::vector<std::string> &arguments) {
   std::vector<Period> periods = {Period{"1H", 60 * 60}, Period{"4H", 4 * 60 * 60}, Period{"1D", 24 * 60 * 60}, Period{"1W", 7 * 24 * 60 * 60}};
-  if (argc > 1 && std::string(argv[1]) == "--dump") {
+  if (arguments.size() < 2) {
+    printUsage();
+    std::exit(EXIT_FAILURE);
+  }
+  const auto filename = arguments[0];
+  const auto action = arguments[1];
+  if (action == "--dump") {
     std::ifstream file(filename, std::ios::binary);
     Record record;
     while (file >> record) {
       std::cout << record.timestamp << ' ' << static_cast<std::underlying_type<Result>::type>(record.result) << '\n';
     }
-    return 0;
   }
-  if (argc > 1 && std::string(argv[1]) == "--stats") {
+  if (action == "--stats") {
     std::ifstream file(filename, std::ios::binary);
     std::vector<Record> records;
     {
@@ -98,27 +111,43 @@ int main(int argc, char **argv) {
       while (file >> record) records.push_back(record);
     }
     std::cout << "Records " << padString(std::to_string(records.size()), 16) << '\n';
-    for (auto period : periods) {
+    for (const auto &period : periods) {
       U64 start = std::time(nullptr) - period.duration;
-      U64 periodSamples = period.duration / samplingInterval;
+      U64 periodSamples = period.duration / RequestInterval.count();
       U64 effectiveSamples = 0;
       U64 successes = 0;
       for (auto record : records) {
         if (record.timestamp >= start) {
           effectiveSamples++;
-          if (record.result == Result::Success) successes++;
+          if (record.result == Result::Success) {
+            successes++;
+          }
         }
       }
       std::cout << "Coverage (" << period.name << ")  " << padString(toString(100.0 * effectiveSamples / static_cast<double>(periodSamples), 5), 3 + 1 + 5) << '\n';
       std::cout << "Uptime   (" << period.name << ")  " << padString(toString(100.0 * successes / static_cast<double>(effectiveSamples), 5), 3 + 1 + 5) << '\n';
     }
-    return 0;
   }
-  bool running = true;
-  while (running) {
-    std::thread helper(run, filename);
-    helper.detach();
-    std::this_thread::sleep_for(std::chrono::milliseconds(samplingInterval * 1000));
+  if (action == "--monitor") {
+    if (arguments.size() != 3) {
+      printUsage();
+      std::exit(EXIT_FAILURE);
+    }
+    const auto url = arguments[2];
+    std::cout << "Monitoring " << url << " and updating " << filename << "." << '\n';
+    while (true) {
+      std::thread helper(run, filename, url);
+      helper.detach();
+      std::this_thread::sleep_for(RequestInterval);
+    }
   }
+}
+
+int main(int argc, char **argv) {
+  std::vector<std::string> arguments;
+  for (int i = 1; i < argc; i++) {
+    arguments.emplace_back(argv[i]);
+  }
+  actionDispatcher(arguments);
   return 0;
 }
